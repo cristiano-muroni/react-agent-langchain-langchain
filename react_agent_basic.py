106529e9222810a026ac9deb216e3c3c15fa6551
Cristiano Muroni <muronidev@gmail.com>
2026-04-15 00:54:18 -0300

    init project

---
+++ react_agent_basic.py
@@ -0,0 +1,12 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+)
+result = llm.invoke("Me mostre algo sobre gatos")
+print(result.content)

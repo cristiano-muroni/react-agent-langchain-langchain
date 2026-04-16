8eed7a3a469081883c03eace909edb045ce28b70
Cristiano Muroni <muronidev@gmail.com>
2026-04-16 00:04:57 -0300

    refactory

--- react_agent_basic.py
+++ react_agent_basic.py
@@ -5,25 +5,18 @@ from langchain_community.tools import TavilySearchResults
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain import hub
 
-# Carrega .env
 load_dotenv()
 
-# Configura o LLM
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash", 
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
-# Configura a Ferramenta
+
 tools = [TavilySearchResults(max_results=2)]
-
-# Puxa o prompt do Hub
 prompt = hub.pull("hwchase17/react")
-
-# CRIAÇÃO DO AGENTE
 agent = create_react_agent(llm, tools, prompt)
 
-# EXECUTOR
 agent_executor = AgentExecutor(
     agent=agent, 
     tools=tools, 
